e35c30e7ad1f5fb2d45f964e6b35b890e10f32fa
Jesse Andrews <jesse@replicate.com>
2023-02-23 16:09:47 +0000

    update the root cli name

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -6,10 +6,8 @@ import (
 
 func NewRootCommand() (*cobra.Command, error) {
 	rootCmd := cobra.Command{
-		Use:   "r8",
-		Short: "replicate.com helpers",
-		Example: `   To run a command inside a Docker environment defined with Cog:
-      $ cog run echo hello world`,
+		Use:           "r8im",
+		Short:         "replicate.com helpers",
 		Version:       "0.0.1",
 		SilenceErrors: true,
 	}
